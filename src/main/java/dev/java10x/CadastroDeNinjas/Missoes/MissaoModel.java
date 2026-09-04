150e3a6e708cda068e1f058ed14142a2a15c16b5
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-04 10:01:53 -0300

    feat: Acrescenta as anotações do Lombok

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,17 +2,25 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class MissaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String missao;
+
+    @Enumerated(EnumType.STRING)
     private DificuldadeMissao dificuldade;
 
     //Uma missão pode ter vários ninjas
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -4,9 +4,15 @@ import jakarta.persistence.*;
 
 import java.util.List;
 import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_ninjas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class NinjaModel {
 
     @Id
@@ -14,6 +20,8 @@ public class NinjaModel {
     private long id;
     private String nome;
     private int idade;
+
+    @Column(unique = true)
     private String email;
 
     //Vários ninjas podem ter apenas uma missão
@@ -21,40 +29,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissaoModel missao;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
